f0ff7e98c1e0db11734d54b1f0eb2d41986c8382
Cassie Nichols <cassienichols@macbooks-MacBook-Pro.local>
2019-08-07 18:17:20 -0600

    homework complete 2

--- src/Account/index.js
+++ src/Account/index.js
@@ -2,14 +2,43 @@ import React, { Component } from 'react';
 
 
 class Account extends Component {
+  constructor(){
+    super();
+    this.textInput = null;
+    this.setTextInputRef = element => {
+      this.textInput = element;
+    }
+    this.focusTextInput = () => {
+      if(this.textInput) this.textInput.focus();
+    }
+    this.state = {
+    balance: 0
+  }
+}
+depositMoney = (e) => {
+  console.log(this.textInput.value, 'this.textInput')
+  this.setState({
+      balance: this.state.balance + parseInt(this.textInput.value)
+  })
+}
+withdrawMoney = (e) => {
+  this.setState({
+    balance: this.state.balance - parseInt(this.textInput.value)
+  })
+}
+
+
+
   render() {
+const isZero = this.state.balance <= 0 ? 'balance zero' : 'balance';
     return (
       <div className="account">
-        <h2>TODO: CHANGE TO PROP</h2>
-        <div className="balance">$0</div>
-        <input type="text" placeholder="enter an amount" />
-        <input type="button" value="Deposit" />
-        <input type="button" value="Withdraw" />
+        <h2>{this.props.name}</h2>
+        <div className={isZero}>{this.state.balance}</div>
+          <input type="text" ref={this.setTextInputRef} placeholder="enter an amount"/>
+          <input type="button" value="Deposit" onClick={this.depositMoney}/>
+          <input type="button" value="Withdraw" onClick={this.withdrawMoney} />
+
       </div>
     )
   }
